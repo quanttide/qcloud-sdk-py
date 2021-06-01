336e078839d8efe829a3502907c1f88367b6f77a
Guo Zhang <zhangguo@quanttide.com>
2021-06-01 18:49:36 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qcloud-sdk-py',
-    version='0.0.1.dev',
+    version='0.0.1',
     packages=find_packages(exclude=["tests", "tests.*"]),
     url='quanttide.com',
     license='Apache 2.0',
