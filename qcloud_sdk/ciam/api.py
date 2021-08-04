9ad77d8908ba5b4cdae44ff70b31098130c20215
Guo Zhang <zhangguo@quanttide.com>
2021-08-04 17:42:54 +0800

    CIAMAPIClient设计API

--- qcloud_sdk/ciam/api.py
+++ qcloud_sdk/ciam/api.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-import os.path
+
+import os
+from urllib.parse import urlencode, quote_plus
 
 import requests
+import jwt
 
 
 # ----- 鉴权客户端API -----
 
 class CIAMAPIClient(object):
-    def __init__(self, client_id, client_secret, custom_portal_name=None, custom_portal_domain=None):
+    def __init__(self, client_id, client_secret, redirect_uri, code_verifier=None, custom_portal_name=None, custom_portal_domain=None):
         """
         访问某个用户目录下的某个应用鉴权API。
 
@@ -19,6 +22,9 @@ class CIAMAPIClient(object):
         # 应用密钥
         self.client_id = client_id
         self.client_secret = client_secret
+        # 应用配置
+        self.redirect_uri = redirect_uri
+        self.code_verifier = code_verifier
         # 用户目录API访问域名
         assert custom_portal_domain or custom_portal_name, '腾讯云平台域名或自建域名必须设置一个'
         self.portal_domain = custom_portal_domain or custom_portal_name + '.portal.tencentciam.com'
@@ -40,7 +46,7 @@ class CIAMAPIClient(object):
         return self.request_ciam_api('GET', '/oauth2/jwks')
 
     @property
-    def odic_jwks(self):
+    def oidc_jwks(self):
         """
         JWT 公钥用于对 JWT 格式的 ID Token 和 Access Token 进行解密。
         JWT 密钥属于用户目录级，在创建用户目录时自动生成，不同用户目录的密钥不同。
@@ -50,7 +56,42 @@ class CIAMAPIClient(object):
             self._oidc_jwks = self.get_oidc_jwks()
         return self._oidc_jwks
 
-    def validate_oidc_id_token(self, id_token):
+    def request_authorization_code(self):
+        pass
+
+    def request_access_token(self, code, mode='pkce'):
+        assert mode in ['pkce', 'code'], '不符合规定的模式'
+        data = {
+            'client_id': self.client_id,
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': self.redirect_uri,
+        }
+        # PKCE模式
+        if mode == 'pkce':
+            assert self.code_verifier, 'PKCE模式下code_verifier不可为空'
+            data['code_verifier'] = self.code_verifier
+        return self.request_ciam_api('POST', '/oauth2/token', data=urlencode(data, quote_via=quote_plus))
+
+    def validate_access_token(self, access_token):
+        pass
+
+    def decode_id_token(self, id_token):
+        """
+        :param id_token:
+        :return:
+        """
+        claims = jwt.decode(id_token, self.oidc_jwks, algorithm='RS256')
+        return claims
+
+    def validate_id_token_claims(self, id_token_claims):
+        """
+        Ref:
+            - https://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation
+
+        :param id_token_claims:
+        :return:
+        """
         pass
 
 
