d32dfdb0653bec8977c099fcc1022f91f0fbcf0b
Guo Zhang <zhangguo@quanttide.com>
2022-03-22 14:36:30 +0800

    setup文件修改格式

--- setup.py
+++ setup.py
@@ -1,16 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='qcloud-sdk-py',
-    version='0.2.0',
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    url='quanttide.com',
-    license='Apache 2.0',
-    author='QuantTide Inc.',
-    author_email='opensource@quanttide.com',
-    description='腾讯云Python服务端SDK',
-    install_requires=[
-        'requests',
-        'dynaconf'
-    ],
-)
+setup()
