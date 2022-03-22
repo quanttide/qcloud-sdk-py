57a65f667a6bde2741f4bf0a692d7d156a5908f5
Guo Zhang <zhangguo@quanttide.com>
2022-03-22 18:29:01 +0800

    bugfix: 修复单元测试批量运行异常问题

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,13 +5,19 @@
 import os
 import unittest
 from unittest import mock
+import importlib
 
 
 class DynaconfTestCase(unittest.TestCase):
     """
-    TODO：充分讨论各种可能的导入方法，把导入配置和使用配置彻底解耦。
+    TODO：
+      - 充分讨论各种可能的导入方法，把导入配置和使用配置彻底解耦。
+      - 从测试必须强制重载（setUp都不可以）的实现来看，目前的实现并不理想，最好是可以使用Dynaconf的机制判断和导入云函数环境变量。
     """
     def test_load_settings(self):
+        # 强制重载模块后重新导入
+        from qcloud_sdk import config
+        importlib.reload(config)
         from qcloud_sdk.config import settings
         # 默认配置
         self.assertTrue(hasattr(settings, 'SMS_SUPPORTED_REGIONS'))
@@ -30,6 +36,9 @@ class DynaconfTestCase(unittest.TestCase):
         'SCF_FUNCTIONNAME': 'fake-scf-function-name',
     })
     def test_load_settings_scf_runtime(self):
+        # 强制重载模块后重新导入
+        from qcloud_sdk import config
+        importlib.reload(config)
         from qcloud_sdk.config import settings
         # 云函数运行环境内置配置取代SDK默认配置
         self.assertEqual('SCF', os.environ['TENCENTCLOUD_RUNENV'])
