309fd2ca3e8330c2f20cb65286d6a1d3a52f31be
Guo Zhang <zhangguo@quanttide.com>
2022-06-01 15:02:02 +0800

    testing: 增加未验证通过文件删除验证。

--- tests/test_cos_api_object.py
+++ tests/test_cos_api_object.py
@@ -15,6 +15,7 @@ class CosObjectAPITestCase(APIClientTestCase):
     """
     对象API单元测试
     """
+
     def setUp(self):
         # 云端对象Key
         self.object_key = settings.COS_TEST_OBJECT_KEY
@@ -29,11 +30,15 @@ class CosObjectAPITestCase(APIClientTestCase):
 
     def test_get_object_with_range(self):
         content_length = 1024
-        response = self.client.get_object(object_key=self.object_key, range_begin=0, range_end=content_length-1)
+        response = self.client.get_object(object_key=self.object_key, range_begin=0, range_end=content_length - 1)
         self.assertEqual(content_length, int(response.headers['Content-Length']))
 
 
-class CosObjectIntegratedAPITestCase(APIClientTestCase):
+class DownloadObjectTestCase(APIClientTestCase):
+    """
+    下载对象到本地文件API（downlaod_object_to_file）单元测试。
+    """
+
     def setUp(self):
         # 云端对象Key
         self.object_key = settings.COS_TEST_OBJECT_KEY
@@ -46,8 +51,6 @@ class CosObjectIntegratedAPITestCase(APIClientTestCase):
 
     def test_download_object_to_file(self):
         """
-        下载对象到本地文件
-
         大文件测试报告 (cos_object_example.txt)：
          - 云端文件大小：75.23MB
          - 本地下载大小：78.9MB
@@ -58,20 +61,38 @@ class CosObjectIntegratedAPITestCase(APIClientTestCase):
                                             remove_unverified_file=False)
         self.assertTrue(os.path.exists(self.object_file_path))
 
-    def test_download_object_to_file_breakpoint_resume(self):
+    def test_download_breakpoint_resume(self):
         """
         断点续传
         """
         # 模拟下载临时文件
         remove_if_exists(self.object_tmp_file_path)
-        response = self.client.get_object(object_key=self.object_key, range_begin=0, range_end=1024*1024-1)
+        response = self.client.get_object(object_key=self.object_key, range_begin=0, range_end=1024 * 1024 - 1)
         response.save_object_to_file(self.object_tmp_file_path)
         self.assertTrue(os.path.exists(self.object_tmp_file_path))
+        existed_size = os.path.getsize(self.object_tmp_file_path)
         # 断点续传
         result = self.client.download_object_to_file(object_key=self.object_key, file_path=self.object_file_path,
                                                      remove_unverified_file=False)
+        # 验证文件是否下载
         self.assertTrue(os.path.exists(self.object_file_path))
+        # 验证断点续传大小是否正确
         self.assertTrue(result['downloaded_size'] < result['file_size'])
+        self.assertEqual(existed_size, result['file_size'] - result['downloaded_size'])
+
+    def test_download_unverified_file(self):
+        """
+        下载无法通过验证的文件，验证是否删除。
+        :return:
+        """
+        # 破坏中间临时文件，模拟验证异常
+        with open(self.object_tmp_file_path, 'wb') as f:
+            f.write(b'1234567890')
+        with self.assertRaises(ValueError) as context:
+            self.client.download_object_to_file(object_key=self.object_key, file_path=self.object_file_path,
+                                                remove_unverified_file=True)
+            self.assertIn('CRC64校验不通过', context.exception)
+        self.assertFalse(os.path.exists(self.object_tmp_file_path))
 
 
 if __name__ == '__main__':
